86c1c982914634e0ee05bbcbdc9d64519b794765
NiuFuyu <me@off.cx>
2022-10-02 19:49:42 +0800

    Create widget.css

---
+++ source/css/widget.css
@@ -0,0 +1,8 @@
+.widget {
+    display: inline-block;
+    overflow: hidden;
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif;
+    font-size: 0;
+    line-height: 0;
+    white-space: nowrap;
+}
