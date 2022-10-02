4eeafa9d319d0d8005dcaf733be595aeb668bb58
NiuFuyu <me@off.cx>
2022-10-02 19:49:10 +0800

    Delete source/js directory

--- source/js/widget.js
+++
@@ -1,8 +0,0 @@
-.widget {
-    display: inline-block;
-    overflow: hidden;
-    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif;
-    font-size: 0;
-    line-height: 0;
-    white-space: nowrap;
-}
