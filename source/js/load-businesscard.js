7d9fdae3648017ea1530f5d5300557e9c8671c29
NiuFuyu <me@off.cx>
2022-11-28 23:06:29 +0800

    chore(config): :wrench: update BusinessCard config

--- source/js/load-businesscard.js
+++ source/js/load-businesscard.js
@@ -1,16 +1,16 @@
 let configs = {
   qrcodeUrl: 'https://off.cx/img/zfb.jpg', // 卡正面的二维码
   invitationText: 'Invitation', // 卡正面左上角的文本
-  cardName: 'NameFlying', // 卡的名字
-  cardId: '114514', // 卡的数字
+  cardName: 'Niute Fony', // 卡的名字
+  cardId: '791486694', // 卡的数字
   endText: 'created:', // 卡正面名字下面的文本
-  endDate: '14/10/2022', // 卡正面名字下面的日期
-  cardHolder: 'Welcome to my blog', // 卡正面左下角的文本
+  endDate: '28/11/2022', // 卡正面名字下面的日期
+  cardHolder: 'Welcome to my channel', // 卡正面左下角的文本
   back1: 'sitelink', // 卡背面文本 1
-  back2: '欢迎加入', // 卡背面文本 2
-  backUrl: '???', // 卡背面文本 2 链接
-  back3: '123', // 卡背面文本 3
-  back4: '4561' // 卡背面文本 4
+  back2: '欢迎加入【Niuteの交流群】', // 卡背面文本 2
+  backUrl: 'https://t.me/NIUTECHAT_CN', // 卡背面文本 2 链接
+  back3: 'Use this card to join the channel and participate in a pleasant discussion together .', // 卡背面文本 3
+  back4: 'Welcome to Niute's channel, wish you a nice day.' // 卡背面文本 4
 }
 
 if (window.location.pathname.startsWith('/posts')) {
